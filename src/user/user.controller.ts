858121b7890e83571e22da45344e0dcdd6c5b140
Milos Vidakovic <milosvidakovic68@gmail.com>
2025-09-17 17:46:20 +0200

    Adding CRUD for users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,61 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   Injectable,
+  Param,
+  ParseIntPipe,
+  Patch,
   UseGuards,
-  Request,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guard';
 import { GetUser } from 'src/auth/decorator';
 import type { User } from '@prisma/client';
+import { EditUserDto } from './dto';
 
 @Injectable()
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  // **************** me ***************************
+
   @UseGuards(JwtAuthGuard)
-  @Get('getMe')
+  @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  editMe(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
+
+  // ***********************************************
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) userId: number) {
+    return this.userService.getUserById(userId);
+  }
+
+  @Delete(':id')
+  deleteUserById(@Param('id', ParseIntPipe) userId: number) {
+    return this.userService.deleteUser(userId);
+  }
+
+  @Patch(':id')
+  editUser(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() dto: EditUserDto,
+  ) {
+    return this.userService.editUser(userId, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
-  getMe() {}
-  getAllUsers() {}
-  getUserById() {}
-  creteUser() {}
-  editUserById() {}
-  deleteUserById() {}
+  constructor(private prisma: PrismaService) {}
+
+  async getAllUsers() {
+    const users = await this.prisma.user.findMany();
+
+    if (users.length === 0) {
+      throw new NotFoundException('No users found in the database.');
+    }
+
+    return users;
+  }
+
+  async getUserById(userId: number) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) throw new NotFoundException(`User with id ${userId} not found.`);
+
+    return user;
+  }
+
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) throw new NotFoundException(`User with id ${userId} not found.`);
+
+    const updatedUser = await this.prisma.user.update({
+      where: { id: userId },
+      data: { ...dto },
+    });
+
+    updatedUser.password = 'NOT_VISABLE_HERE';
+    return updatedUser;
+  }
+
+  async deleteUser(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException(`User with id ${userId} not found.`);
+
+    await this.prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+
+    return { message: `User with id ${userId} was sucessfuly deleted!` };
+  }
 }
